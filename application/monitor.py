4c8bd8dfd4607ef3c6a9955f11ea6c850c9b49ef
Faizun Musthofa <gtasu123@gmail.com>
2025-04-19 17:00:51 +0700

    v1.1b

--- application/monitor.py
+++ application/monitor.py
@@ -1,42 +1,32 @@
 from core.memory.flash import Flash
 from core.program_counter import ProgramCounter
+from core.instructions.instruction_decoder import InstructionDecoder
 
 
 class Monitor:
     def __init__(self, flash: Flash, PC: ProgramCounter):
         self.PC: ProgramCounter = PC
         self.flash: Flash = flash
+        self.Decoder: InstructionDecoder = InstructionDecoder()
 
     def show(self):
-        offsett = self.PC.address // 8
         print(f"On Process: {format(self.flash.get(self.PC.address), 'b').zfill(16)}")
 
         if self.PC.address > self.PC.address + 32:
             raise "Out of memory"
-        print("\n      ADDRESS     -->  ", end="")
-        for i in range(0, 16):
-            print(format(i, "X").zfill(2), end=" ")
-        print("")
-        print("                       ", end="")
-        for i in range(0, 8):
-            print(
-                f"{'__ __' if (self.PC.address % 8) == i else ''}",
-                end="      ",
-            )
-        print("")
+        print("\n      ADDRESS     -->  OPCODE            -->  Definition\n")
 
-        for y in range(0 + (offsett * 8), 32 + (offsett * 8), 8):
+        for y in range(0 + self.PC.address, 5 + self.PC.address):
             addrs = format(y, "X").zfill(8)
 
             print(
-                f"{' --> ' if y in range((self.PC.address - 8), (self.PC.address - 8) + 8) or y == 0 + (offsett * 8) else '     '} 0x{addrs}  -->  ",
+                f"{' --> 'if self.PC.address == y else '     '} 0x{addrs}  -->  ",
                 end="",
             )
-            for x in range(y, y + 8):
-                try:
-                    hexval = format(self.flash.get(x), "X").zfill(4)
-                    print(hexval[2:], end=" ")
-                    print(hexval[:2], end=" ")
-                except:
-                    print("      ", end="")
-            print("")
+            hexval = format(self.flash.get(y), "b").zfill(16)
+            print(hexval, end="")
+            try:
+                definition = self.Decoder.decode(self.flash.get(y), True)
+                print(f"  -->  {definition}")
+            except Exception as e:
+                print(f"  -->  {e}")
--- core/instructions/instruction_decoder.py
+++ core/instructions/instruction_decoder.py
@@ -2,54 +2,70 @@ from core.alu import ALU
 
 
 class InstructionDecoder:
-    def __init__(self, PC, alu):
+    def __init__(self, PC=None, alu=None):
         self.alu: ALU = alu
         self.PC = PC
 
-    def decode(self, operation: int):
+    def decode(self, operation: int, definition=False):
         if operation == 0:
-            self.PC.address += 1
-            return
+            if not definition:
+                self.PC.address += 1
+                return
+            else:
+                return "NOP"
         # ADC
-        if (operation >> 10) & 0b111111 == 0b000111:
+        elif (operation >> 10) & 0b111111 == 0b000111:
             destination = (operation >> 4) & 0b11111
             high = (operation >> 9) & 0b1
             low = (operation) & 0b1111
             source = (high << 5) | low
-            self.alu.ADC(destination, source)
-            self.PC.address += 1
+            if not definition:
+                self.alu.ADC(destination, source)
+                self.PC.address += 1
+            return f"ADC R{source}, {source}"
         # ADD
         elif (operation >> 10) & 0b111111 == 0b000011:
             destination = (operation >> 4) & 0b11111
             high = (operation >> 9) & 0b1
             low = (operation) & 0b1111
             source = (high << 5) | low
-            self.alu.ADD(destination, source)
-            self.PC.address += 1
+            if not definition:
+                self.alu.ADD(destination, source)
+                self.PC.address += 1
+            return f"ADD R{source}, {source}"
         # SUB
         elif (operation >> 10) & 0b111111 == 0b000010:
             destination = (operation >> 4) & 0b11111
             high = (operation >> 9) & 0b1
             low = (operation) & 0b1111
             source = (high << 5) | low
-            self.alu.SUB(destination, source)
-            self.PC.address += 1
+            if not definition:
+                self.alu.SUB(destination, source)
+                self.PC.address += 1
+            return f"SUB R{source}, {source}"
         # SBC
         elif (operation >> 10) & 0b111111 == 0b000110:
             destination = (operation >> 4) & 0b11111
             high = (operation >> 9) & 0b1
             low = (operation) & 0b1111
             source = (high << 5) | low
-            self.alu.SUB(destination, source)
-            self.PC.address += 1
+            if not definition:
+                self.alu.SBC(destination, source)
+                self.PC.address += 1
+            return f"SBC R{source}, {source}"
         # LDI
         elif (operation >> 12) & 0b1111 == 0b1110:
             destination = (operation >> 4) & 0b1111
             high = (operation >> 8) & 0b1111
             low = (operation) & 0b1111
             source = (high << 4) | low
-            self.alu.LDI(destination, source)
-            self.PC.address += 1
+            if not definition:
+                self.alu.LDI(destination, source)
+                self.PC.address += 1
+            return f"LDI R{source}, {source}"
         else:
-            self.PC.address += 1
-            raise ValueError("Unknown OPCODE")
+            if not definition:
+                self.PC.address += 1
+                raise ValueError("Unknown OPCODE")
+            else:
+                return "Unknown OPCODE"
